b07349a34122fb2efc8dc788f68add6c652ca013
Moustapha <mouhamedjr2012@gmail.com>
2023-05-04 18:05:12 +0000

    first-commit

---
+++ src/redux.js
@@ -0,0 +1,34 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const todoSlice = createSlice({
+  name: "todo",
+  initialState: [
+    { id: 1, text: "Faire les courses", done: false },
+    { id: 2, text: "Ménage !", done: true },
+  ],
+  reducers: {
+    addTask: (state, action) => {
+      const newTask = {
+        id: Date.now(),
+        done: false,
+        text: action.payload,
+      };
+      state.push(newTask);
+    },
+    toggleTask: (state, action) => {
+      const task = state.find((t) => t.id === action.payload);
+      task.done = !task.done;
+    },
+    deleteTask: (state, action) => {
+      state.filter((t) => t.id !== action.payload);
+      return state;
+    },
+  },
+});
+
+export const { addTask, deleteTask, toggleTask } = todoSlice.actions;
+export const store = configureStore({
+  reducer: {
+    todo: todoSlice.reducer,
+  },
+});
